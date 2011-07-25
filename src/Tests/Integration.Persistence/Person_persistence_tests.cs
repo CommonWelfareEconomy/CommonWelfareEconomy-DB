7442e5b6aca65a979090b18b58a39b55c0518390
Robert Mischke <robertmischke@gmail.com>
2011-07-25 18:36:37 +0200

    Initial Person_persistence_tests

---
+++ src/Tests/Integration.Persistence/Person_persistence_tests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GwoDb.Tests.Integration
+{
+
+    public class Person_persistence_tests
+    {
+    }
+}
