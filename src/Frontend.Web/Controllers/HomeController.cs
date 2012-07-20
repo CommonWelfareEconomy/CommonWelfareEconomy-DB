dd08396eacae79648c93cc40779325ec9125ebe3
Robert Mischke <robertmischke@gmail.com>
2012-07-20 20:40:31 +0200

    minor (computer change)

--- src/Frontend.Web/Controllers/HomeController.cs
+++ src/Frontend.Web/Controllers/HomeController.cs
@@ -1,20 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace Frontend.Web.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
-        {
-            ViewBag.Message = "Welcome to ASP.NET MVC!";
-
-            return View();
-        }
-
         public ActionResult About()
         {
             return View();
---
+++ src/Frontend.Web/Controllers/Search/Company/Edit/CompanyDetailsModel.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+using DataAnnotationsExtensions;
+using GwoDb;
+
+public class CompanyDetailsModel : Controller
+{
+    public Message Message;
+
+    [Required]
+    [Display(Name = "Firmenname")]
+    public string CompanyName { get; set; }
+
+    [Required]
+    [Display(Name = "Branche")]
+    public string Industry { get; set; }
+
+    [Required]
+    [Display(Name = "Mitarbeiterzahl")]
+    public string Size { get; set; }
+
+    [Required]
+    [Display(Name = "Webseite")]
+    [Url(requireProtocol: false)]
+    public string Url { get; set; }
+
+    [Display(Name = "Ort")]
+    public string Location { get; set; }
+
+    [Display(Name = "Plz")]
+    public string ZipCode { get; set; }
+
+    [Required]
+    [Display(Name = "E-Mail")]
+    [Email]
+    public string Email { get; set; }
+
+    [Required]
+    [Display(Name = "Bilanz für 2012")]
+    public bool WelfareBalanceFor2012 { get; set; }
+
+    [Required]
+    [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+    [DataType(DataType.Password)]
+    [Display(Name = "Password")]
+    public string Password { get; set; }
+}
